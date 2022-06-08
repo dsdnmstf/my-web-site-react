f0787748843f76691e8812f4b07583f0c529595d
musrafa <dsdnmstf@gmail.com>
2022-06-08 21:25:07 +0200

    pages added

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,22 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MyNavbar from "../components/MyNavbar";
+import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
 import Register from "../pages/Register";
-import PrivateRouter from "./PrivateRouter";
+
 const AppRouter = () => {
   return (
     <Router>
-      <MyNavbar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/details/:id" element={<PrivateRouter />}>
-          <Route path="" element={<MovieDetail />} />
-        </Route>
-      </Routes>
+      <Navbar />
+      <Routes>{/* <Route path="/" element={<Main />} /> */}</Routes>
     </Router>
   );
 };
