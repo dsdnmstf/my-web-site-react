00e6cfb59ab7af587dd3985770ff41f10ad46176
musrafa <dsdnmstf@gmail.com>
2022-06-08 21:22:09 +0200

    router added

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const PrivateRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+  return currentUser ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ isLogin: true }} />
+  );
+};
+
+export default PrivateRouter;
